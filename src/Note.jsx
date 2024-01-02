a6e0ec7126716c0e13f42307b417da137c39876c
fuadfatali1 <fuad0fataliyev0@outlook.com>
2024-01-02 13:16:43 +0400

    give flexbox

--- src/Note.jsx
+++ src/Note.jsx
@@ -4,10 +4,12 @@ import ReactDOM from "react-dom"
 function Note(props){
    
     return (
+        <div className="note-container">
        <div className="note">  
         <h1>{props.answer}</h1>
         
         <p>{props.question}</p>
+       </div>
        </div>
     );
 }
